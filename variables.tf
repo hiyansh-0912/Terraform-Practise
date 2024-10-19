1452d6d6fc3e9720fdad05018df07b2c3c4be47e
hiyansh-0912 <harilokanadam@gmail.com>
2024-10-19 13:53:49 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "cidr_block" {
+    type = list
+  
+}
+variable "Hari" {
+    type = map
+    default = {
+      sub-1 = "192.168.0.0/24",
+      sub-2 = "172.10.0.0/24"
+      
+      
+    }
+  
+}
